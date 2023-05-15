5895fd48ad82e295a3b5fe97cad833b11883ecd7
EvanilsonP <pevanilson99@gmail.com>
2023-05-14 21:10:42 -0300

    Fetching info from website

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ const PORT = process.env.PORT || 3000;
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const url = 'https://www.imdb.com/chart/top/';
+
+axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html);
+            const articles = [];
+
+            $('.titleColumn', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).find('a').attr('href');
+                articles.push({ title, url });
+                console.log(articles);
+            })
+        }).catch(err => console.log(err))
+
 app.listen(PORT, () => console.log(`Up and running on PORT ${PORT}`));
